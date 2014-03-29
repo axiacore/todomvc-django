935050ad79dafaf67cd65c3c24af08bbce663015
Pablo Vallejo <pabloninety@gmail.com>
2014-03-29 18:23:32 -0500

    Use class based views as default views.

--- api/views.py
+++ api/views.py
@@ -11,92 +11,86 @@ from api.serializers import TaskSerializer
 # Example using function based views
 # -----------------------------------
 
-@api_view(['GET', 'POST'])
-def task_list(request):
-    """
-    List all task, or create a new one
-    """
+# @api_view(['GET', 'POST'])
+# def task_list(request):
+#     """
+#     List all task, or create a new one
+#     """
 
-    # GET Request
-    if request.method == 'GET':
-        tasks = Task.objects.all()
-        serializer = TaskSerializer(tasks)
-        return Response(serializer.data)
+#     # GET Request
+#     if request.method == 'GET':
+#         tasks = Task.objects.all()
+#         serializer = TaskSerializer(tasks)
+#         return Response(serializer.data)
 
-    # POST Request
-    if request.method == 'POST':
-        serializer = TaskSerializer(data=request.DATA)
+#     # POST Request
+#     if request.method == 'POST':
+#         serializer = TaskSerializer(data=request.DATA)
 
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-        else:
-            return Response(
-                serializer.errors, status=status.HTTP_400_BAD_REQUEST
-            )
+#         else:
+#             return Response(
+#                 serializer.errors, status=status.HTTP_400_BAD_REQUEST
+#             )
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def task_detail(request, pk):
-    """
-    Get, update, or delete a specific task
-    """
-    try:
-        task = Task.objects.get(pk=pk)
-    except Task.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+# @api_view(['GET', 'PUT', 'DELETE'])
+# def task_detail(request, pk):
+#     """
+#     Get, update, or delete a specific task
+#     """
+#     try:
+#         task = Task.objects.get(pk=pk)
+#     except Task.DoesNotExist:
+#         return Response(status=status.HTTP_404_NOT_FOUND)
 
-    # GET request
-    if request.method == 'GET':
-        serializer = TaskSerializer( task )
-        return Response( serializer.data )
+#     # GET request
+#     if request.method == 'GET':
+#         serializer = TaskSerializer( task )
+#         return Response( serializer.data )
 
-    # PUT request
-    if request.method == 'PUT':
-        serializer = TaskSerializer(task, data=request.DATA)
+#     # PUT request
+#     if request.method == 'PUT':
+#         serializer = TaskSerializer(task, data=request.DATA)
 
-        if serializer.is_valid():
-            serializer.save()
-            return Response(status=status.HTTP_201_CREATED)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(status=status.HTTP_201_CREATED)
 
-        else:
-            return Response(
-                serializer.errors, status=status.HTTP_400_BAD_REQUEST
-            )
+#         else:
+#             return Response(
+#                 serializer.errors, status=status.HTTP_400_BAD_REQUEST
+#             )
 
-    # DELETE request
-    elif request.method == 'DELETE':
-        task.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+#     # DELETE request
+#     elif request.method == 'DELETE':
+#         task.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 
 # Example using class based views
 # -----------------------------------
-# class TaskMixin(object):
-#     """
-#     Mixin to inherit from.
-#     Here we're setting the query set and the serializer
-#     """
-#     queryset = Task.objects.all()
-#     serializer_class = TaskSerializer
+class TaskMixin(object):
+    """
+    Mixin to inherit from.
+    Here we're setting the query set and the serializer
+    """
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
 
-# class TaskList(TaskMixin, ListCreateAPIView):
-#     """
-#     Return a list of all the tasks, or
-#     create new ones
-#     """
-#     pass
+class TaskList(TaskMixin, ListCreateAPIView):
+    """
+    Return a list of all the tasks, or
+    create new ones
+    """
+    pass
 
-# class TaskDetail(TaskMixin, RetrieveUpdateDestroyAPIView):
-#     """
-#     Return a specific task, update it, or delete it.
-#     """
-#     pass
-
-
-
-
-
-
+class TaskDetail(TaskMixin, RetrieveUpdateDestroyAPIView):
+    """
+    Return a specific task, update it, or delete it.
+    """
+    pass
